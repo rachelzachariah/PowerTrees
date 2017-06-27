5f1121335b7ff690880608a9578f6290b4a5bf82
Zachary Charles <zcharles8@gmail.com>
2017-06-27 09:34:14 -0500

    Added phc_script writer

---
+++ write_phc.py
@@ -0,0 +1,22 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-s', type=str, dest="s", default = "")
+parser.add_argument('-w', type=str, dest="w", default = "")
+parser.add_argument('-reps', type=int, dest="reps", default = 10)
+
+
+args = vars(parser.parse_args())
+reps = args["reps"]
+phc_filename = args["w"]
+script_filename = args["s"]
+
+f = open('phc_eqs/'+str(s)+'.sh','w')
+for t in range(reps):
+	f.write('../.././phc -b '+str(phc_filename)+'_'+str(t)+'_eqs.txt '+str(phc_filename)+'_'+str(t)+'_roots.txt')
+
+
+
+
+
+
