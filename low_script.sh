591a2f9e1d23c65dd9c983a1891f1dc94c1ce2e5
Zachary Charles <zcharles8@gmail.com>
2017-06-27 10:15:09 -0500

    Added low script

---
+++ low_script.sh
@@ -0,0 +1,10 @@
+sage random_script.sage -n 3 -md 2 -reps 10 -w lown3md2
+sage random_script.sage -n 4 -md 2 -reps 10 -w lown4md2
+sage random_script.sage -n 4 -md 2 -reps 10 -w lown5md2
+sage random_script.sage -n 6 -md 2 -reps 10 -w lown6md2
+sage random_script.sage -n 7 -md 2 -reps 10 -w lown7md2
+sage random_script.sage -n 8 -md 3 -reps 10 -w lown8md3
+sage random_script.sage -n 9 -md 3 -reps 10 -w lown9md3
+sage random_script.sage -n 10 -md 3 -reps 10 -w lown10md3
+sage random_script.sage -n 11 -md 3 -reps 10 -w lown11md3
+sage random_script.sage -n 12 -md 3 -reps 10 -w lown12md3
