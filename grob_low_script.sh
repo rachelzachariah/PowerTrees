fc9bb4f8cfc6bf83ca579f53db0f96dc59011cc5
Zachary Charles <zcharles8@gmail.com>
2017-07-05 14:33:14 -0500

    Updated GB scripts

--- grob_low_script.sh
+++ grob_low_script.sh
@@ -1,14 +1,14 @@
-sage random_script.sage -n 3 -md 2 -reps 10 -w gln3md2 -g
-sage random_script.sage -n 4 -md 2 -reps 10 -w gln4md2 -g
-sage random_script.sage -n 5 -md 2 -reps 10 -w gln5md2 -g
-sage random_script.sage -n 6 -md 2 -reps 10 -w gln6md2 -g
-sage random_script.sage -n 7 -md 2 -reps 10 -w gln7md2 -g
-sage random_script.sage -n 8 -md 3 -reps 10 -w gln8md3 -g
-sage random_script.sage -n 9 -md 3 -reps 10 -w gln9md3 -g
-sage random_script.sage -n 10 -md 3 -reps 10 -w gln10md3 -g
-sage random_script.sage -n 11 -md 3 -reps 10 -w gln11md3 -g
-sage random_script.sage -n 12 -md 3 -reps 10 -w gln12md3 -g
-sage random_script.sage -n 13 -md 3 -reps 10 -w gln13md3 -g
-sage random_script.sage -n 14 -md 3 -reps 10 -w gln14md3 -g
-sage random_script.sage -n 15 -md 3 -reps 10 -w gln15md3 -g
-sage random_script.sage -n 16 -md 3 -reps 10 -w gln16md3 -g
+sage random_script.sage -n 3 -md 2 -reps 10 -g
+sage random_script.sage -n 4 -md 2 -reps 10 -g
+sage random_script.sage -n 5 -md 2 -reps 10 -g
+sage random_script.sage -n 6 -md 2 -reps 10 -g
+sage random_script.sage -n 7 -md 2 -reps 10 -g
+sage random_script.sage -n 8 -md 3 -reps 10 -g
+sage random_script.sage -n 9 -md 3 -reps 10 -g
+sage random_script.sage -n 10 -md 3 -reps 10 -g
+sage random_script.sage -n 11 -md 3 -reps 10 -g
+sage random_script.sage -n 12 -md 3 -reps 10 -g
+sage random_script.sage -n 13 -md 3 -reps 10 -g
+sage random_script.sage -n 14 -md 3 -reps 10 -g
+sage random_script.sage -n 15 -md 3 -reps 10 -g
+sage random_script.sage -n 16 -md 3 -reps 10 -g
